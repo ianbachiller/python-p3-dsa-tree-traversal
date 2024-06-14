c6e32be3b60e061d7c394a911b9c3bc98b2987dd
Ian Bachiller <ianbachise@gmail.com>
2024-06-14 04:19:53 -0400

    completed traversal dsa lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,16 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    if not self.root:
+        return None
+    def dfs(node):
+        if node['id'] == id:
+            return node
+        
+        for child in node['children']:
+            result = dfs(child)
+            if result:
+                return result
+        
+        return None
+    return dfs(self.root)
